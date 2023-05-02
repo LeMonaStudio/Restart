75b48a1e2909c1dc0c511a49d2a9228d5d6f1ca3
Musa Lawrence Ilemona <musa.lawrence93@gmail.com>
2023-05-02 13:29:53 +0100

    Onboarding screen: header section completed.

--- Restart/Screens/OnboardingView.swift
+++ Restart/Screens/OnboardingView.swift
@@ -11,10 +11,28 @@ struct OnboardingView: View {
     @AppStorage("onboarding") var isOnboardingViewActive: Bool = true
     
     var body: some View {
-        
         ZStack {
             Color("ColorBlue").ignoresSafeArea()
+            //MARK: - HEADER
+            Spacer()
+            VStack(spacing: 0) {
+                Text("Share.")
+                    .font(.system(size: 60))
+                    .fontWeight(.heavy)
+                    .foregroundColor(.white)
+                Text("""
+                It's not how much we give
+                but how much love we put into giving.
+                """)
+                .font(.title3)
+                .fontWeight(.light)
+                .foregroundColor(.white)
+                .multilineTextAlignment(.center)
+                .padding(.horizontal, 10)
+            }
+            //MARK: - CENTER
             
+            //MARK: - FOOTER
         }
     }
 }
