57503d48e2345949adb927718bae9d2913c2f05c
Musa Lawrence Ilemona <musa.lawrence93@gmail.com>
2023-05-02 09:39:47 +0100

    Initial Commit

---
+++ Restart/RestartApp.swift
@@ -0,0 +1,17 @@
+//
+//  RestartApp.swift
+//  Restart
+//
+//  Created by Musa Lawrence on 5/2/23.
+//
+
+import SwiftUI
+
+@main
+struct RestartApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
